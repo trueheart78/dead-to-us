66567b458ebb95b2b807a2a1982fc201ebfc9756
Josh Mills <jmills@covermymeds.com>
2017-10-16 10:32:13 -0400

    Basic new welcome home page setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#home'
+  get 'welcome/home'
+  get 'welcome/about' unless Rails.env.production?
 end
