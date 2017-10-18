330b71ca413598ddc753d21098ed70b272547ff0
Josh Mills <josh@trueheart78.com>
2017-10-18 13:34:17 -0400

    grave layout

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,7 +1,7 @@
 module WelcomeHelper
   def records
     [
-      { name: 'Mark G', death_date: '2017', epitaph: 'even if you couldn\'t handle being the only Mark.'},
+      { name: 'Mark G', death_date: '2017', epitaph: 'product ownership got the best of you.'},
       { name: 'Mark H', death_date: '2017', epitaph: 'you flew the coop too soon.'},
       { name: 'Jake', death_date: '2017', epitaph: 'your insurance did not cover this. #StateFarm'},
       { name: 'Barry', death_date: '2017', epitaph: 'something something a "your face" joke.'},
@@ -10,10 +10,10 @@ module WelcomeHelper
 
   def epitaph_intro
     [
-      'Here lies',
       'Goodbye',
       'Farewell',
-      'We miss you'
+      'We miss you',
+      'So long'
     ].sample
   end
 end
