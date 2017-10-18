fa1c5a007b5b9c04fa5bb217af843b46188125d1
Josh Mills <josh@trueheart78.com>
2017-10-18 13:48:08 -0400

    grave update

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -3,17 +3,28 @@ module WelcomeHelper
     [
       { name: 'Mark G', death_date: '2017', epitaph: 'product ownership got the best of you.'},
       { name: 'Mark H', death_date: '2017', epitaph: 'you flew the coop too soon.'},
-      { name: 'Jake', death_date: '2017', epitaph: 'your insurance did not cover this. #StateFarm'},
+      { name: 'Jake', death_date: '2017', epitaph: 'your insurance apparently did not cover this. #StateFarm'},
       { name: 'Barry', death_date: '2017', epitaph: 'something something a "your face" joke.'},
     ]
+  end
+
+  def epitaph(record)
+    [ epitaph_intro, relationship ].compact.join(' ') + ', ' + record.fetch(:epitaph)
   end
 
   def epitaph_intro
     [
       'Goodbye',
       'Farewell',
-      'We miss you',
-      'So long'
+      'So long',
+    ].sample
+  end
+
+  def relationship
+    [
+      'friend',
+      'pal',
+      'buddy'
     ].sample
   end
 end
