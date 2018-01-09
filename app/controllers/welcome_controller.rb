9223badf8599034f8f8ea0734a48606e991b1517
Dan <dmeehan@covermymeds.com>
2018-01-08 19:28:01 -0500

    Add support for themes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,21 @@
 class WelcomeController < ApplicationController
   def home
+    respond_to do |type|
+      type.html { render }
+      type.all  { unprocessable_entity }
+    end
   end
 
   def about
+    respond_to do |type|
+      type.html { render }
+      type.all  { unprocessable_entity }
+    end
+  end
+
+  private
+
+  def unprocessable_entity
+    render plain: '422', status: 422
   end
 end
--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,32 @@
 module WelcomeHelper
+  def epitaph(record)
+    [ epitaph_intro, relationship ].compact.join(' ') + ', ' + record.fetch(:epitaph)
+  end
+
+  def epitaph_intro
+    [
+      'Goodbye',
+      'Farewell',
+      'So long'
+    ].sample
+  end
+
+  def relationship
+    [
+      'friend',
+      'pal',
+      'buddy'
+    ].sample
+  end
+
+  def records
+    [
+      { name: 'Josh M', icon: :zombie_boy, death_date: 2018, epitaph: 'never knew Taylor Swift gifs could be so deadly. Just don\'t forget to keep maintaining this site.' },
+      { name: 'Matt W', icon: :vampire_boy, death_date: 2017, epitaph: 'it\'s pronounced "gif" with a soft "g".'},
+      { name: 'Mark G', icon: :coffin, death_date: 2017, epitaph: 'product ownership got the best of you.'},
+      { name: 'Mark H', icon: :coffin, death_date: 2017, epitaph: 'you flew the coop too soon.'},
+      { name: 'Jake T', death_date: 2017, epitaph: 'your insurance apparently did not cover this. #StateFarm'},
+      { name: 'Barry B', death_date: 2017, epitaph: 'something something a "your face" joke.'},
+    ]
+  end
 end
